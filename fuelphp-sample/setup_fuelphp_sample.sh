e14d319de5422ea7b1a40a04295e998fbbbd8750
Takuya KIHARA <gray@tacck.net>
2015-11-03 19:02:05 +0900

    Use curl. Change cp option.

--- fuelphp-sample/setup_fuelphp_sample.sh
+++ fuelphp-sample/setup_fuelphp_sample.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget -O fuelphp-1.7.3.zip http://fuelphp.com/files/download/34
+curl -o fuelphp-1.7.3.zip http://fuelphp.com/files/download/34
 unzip fuelphp-1.7.3.zip
 mv fuelphp-1.7.3 fuelphp
-cp -R sample/fuelphp/ fuelphp
+cp -R sample/fuelphp .
